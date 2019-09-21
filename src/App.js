6bbcc03cadb332ba33342b0eb0a7012b6d9663f6
María Mata <matah.maria@gmail.com>
2019-09-22 01:37:03 +0200

    request made in a new service, saved in the state and rendered

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
 import React from 'react';
- 
 import './App.css';
+import {fetchPokemones} from './services/fetchPokemons';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {}
+    
+    this.state = {
+      pokemons : []
+    }
   }
-
+  
+  // hacer la petición aquí cuando sean automáticas
+  componentDidMount() {
+    this.getPokemons ();
+  }
+  
+  getPokemons(){
+    fetchPokemones()
+    .then(data => {
+      this.setState({
+        pokemons: data
+      })
+    })
+  }
+  
   render() {
+    console.log('Me estoy pintando', this.state.pokemons.length);
     return (
       <div className="app">
-      </div>
-    );
-  }
-}
-
-export default App;
+      <h1>Mi lista de Pokemons</h1>
+      <ul>
+      {this.state.pokemons.map(item => {
+        return(
+          <li key={item.id}>
+            <div>
+              <h2>{item.name}</h2>
+                <div>
+                  <img src={item.url} alt= {`Imagen de ${item.name}`}/>
+                </div>
+            </div>
+          </li>
+          )
+        })}
+        </ul>
+        </div>
+        );
+      }
+    }
+    
+    export default App;
+    
---
+++ src/services/fetchPokemons.js
@@ -0,0 +1,7 @@
+const ENDPOINT = 'https://raw.githubusercontent.com/Adalab/m3-evaluacion-intermedia-oneeyedman/componentbranch/pokemons.json'
+
+const fetchPokemones = () => {
+    return fetch(ENDPOINT).then(res => res.json())
+}
+
+export {fetchPokemones};
